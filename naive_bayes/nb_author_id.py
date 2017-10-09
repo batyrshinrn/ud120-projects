51dd6af2974ff493375d9dd48c7bd7af79d75bde
Ramil Batyrshin <v-rabaty@microsoft.com>
2017-10-09 12:12:58 +0300

    hw: naive_bayes + svm + decision_tree

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -21,12 +21,22 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
 
+t1 = time()
+pred = clf.predict(features_test)
+print "predicting time:", round(time()-t1, 3), "s"
 
+from sklearn.metrics import accuracy_score
+acurancy = accuracy_score(pred, labels_test)
+print(acurancy)
 
 #########################################################
 ### your code goes here ###
-
 
 #########################################################
 
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -20,11 +20,34 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
 ### your code goes here ###
 
 #########################################################
+from sklearn.svm import SVC
+clf = SVC(kernel="rbf", C=10000.0)
 
+# features_train = features_train[:len(features_train)/100] 
+# labels_train = labels_train[:len(labels_train)/100] 
 
+t0 = time()
+print "start trining"
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+print "start predict"
+pred = clf.predict(features_test)
+print "predicting time:", round(time()-t1, 3), "s"
+print "elem 10: ", pred[10]
+print "elem 26: ", pred[26]
+print "elem 50: ", pred[50]
+
+count = sum(pred)
+print "Chris count ", count 
+
+print("calc accurancy")
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+
+print(acc)
