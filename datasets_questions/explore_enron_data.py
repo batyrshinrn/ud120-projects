6be19cfbbbdcd33f7fbc8b01d08f75efb8c81ff3
Ramil Batyrshin (Akvelon) <v-rabaty@microsoft.com>
2017-10-20 19:40:42 +0300

    data

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,52 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+maxTotal_payments = 0
+employee = ""
+quantifiedSalary = 0
+quantifiedEmail = 0
+totalPaymentsNaN = 0
+totalPayments = 0
+totalPoi = 0
+
+for key, value in enron_data.items():
+    if key != "TOTAL" and value["total_payments"] != "NaN" and value["total_payments"] > maxTotal_payments:
+        maxTotal_payments = value["total_payments"]
+        employee = key
+    if value["salary"] != "NaN":
+        quantifiedSalary += 1
+    if value["email_address"] != "NaN":
+        quantifiedEmail += 1
+    if value["total_payments"] == "NaN":
+        totalPaymentsNaN += 1
+    if value["poi"] == True and value["total_payments"] == "NaN":
+        totalPayments += 1
+    if value["poi"] == True:
+        totalPoi += 1
+
+print len(enron_data.keys())
+print maxTotal_payments
+print employee
+print quantifiedSalary
+print quantifiedEmail
+print "Total number of total payments == NaN: " + str(totalPaymentsNaN)
+print "Total POI's with total payments == NaN: " + str(totalPayments)
+print "Total POI's: " + str(totalPoi)
+
+import sys
+sys.path.append("../tools/")
+from feature_format import featureFormat
+from feature_format import targetFeatureSplit
+
+feature_list = ["poi", "salary", "bonus"] 
+data_array = featureFormat( enron_data, feature_list )
+label, features = targetFeatureSplit(data_array)
+
+print enron_data["LAY KENNETH L"]
+
+# print label
+# print features
+
+#salary email_address
+
 
