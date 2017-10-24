a23859de3d64cd09452564115f3c95dc2b0f0413
Ramil Batyrshin (Akvelon) <v-rabaty@microsoft.com>
2017-10-24 12:40:01 +0300

    regression + outliers

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -6,14 +6,40 @@ import matplotlib.pyplot
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
+def getBonuses(seq):
+    for el in seq:
+        if el["bonus"] == "NaN":
+            yield 0
+        else:
+            yield el["bonus"]
 
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 features = ["salary", "bonus"]
+
+maxBonus = max(getBonuses(data_dict.values()))
+print "max bonus: ", maxBonus
+outlier = [ k for k, v in data_dict.iteritems() if v["bonus"] == maxBonus ].pop()
+print outlier
+data_dict.pop(outlier, 0)
+
+print [k for k, v in data_dict.iteritems() if v["bonus"] != "NaN" and v["bonus"] > 5000000 and v["salary"] > 1000000]
+
+
+
 data = featureFormat(data_dict, features)
 
 
 ### your code below
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
+
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
+
 
 
 
--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -11,10 +11,31 @@ def outlierCleaner(predictions, ages, net_worths):
         each tuple is of the form (age, net_worth, error).
     """
     
-    cleaned_data = []
+    to_remove = getmaxerrors(predictions, net_worths, 10)
+    print "Remove inexes: ", to_remove
 
-    ### your code goes here
-
+    cleaned_data = [(age[0], net_worths[i][0], predictions[i][0] - net_worths[i][0]) for i, age in enumerate(ages) if i not in to_remove]
     
     return cleaned_data
+
+
+def getmaxerrors(predictions, net_worths, portion):
+   
+    count = int(len(predictions)*portion/100)
+    maxerrors = []
+    for i in range(count):
+        maxerrors.append({ "err": 0, "index": 0})
+    
+    for i, item in enumerate(predictions):
+        c_err = (predictions[i][0] - net_worths[i][0])**2
+        appendmax(maxerrors, c_err, i)        
+
+    return map(lambda x:x["index"], maxerrors)
+
+def appendmax(array, value, i):
+   result = min(array, key=lambda x:x['err'])
+   if(result["err"] < value):
+        result["err"] = value
+        result["index"] = i
+
 
